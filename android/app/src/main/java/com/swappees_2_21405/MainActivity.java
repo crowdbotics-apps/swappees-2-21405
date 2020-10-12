894447b9760641658a718f73899a98be8e306025
Crowdbotics <team@crowdbotics.com>
2020-10-12 17:10:51 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/swappees_2_21405/MainActivity.java
@@ -0,0 +1,15 @@
+package com.swappees_2_21405;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "swappees_2_21405";
+    }
+}
